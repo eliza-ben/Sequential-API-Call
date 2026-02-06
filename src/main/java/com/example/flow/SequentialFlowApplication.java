9a160a137d8141f4a99c10ac238cb3fe8eabaef4
eliza-ben <elizabennette15@gmail.com>
2026-02-06 13:17:30 -0500

    Create SequentialFlowApplication.java

---
+++ src/main/java/com/example/flow/SequentialFlowApplication.java
@@ -0,0 +1,11 @@
+package com.example.flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SequentialFlowApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(SequentialFlowApplication.class, args);
+  }
+}
